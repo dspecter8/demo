e73240c88048c79b329e1a5087cbe5c1a0a7418c
FNE1 <FNE1@LP5-FNE1-1DSY.dsone.3ds.com>
2019-12-16 13:04:06 +0100

    First Commit

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.dao.ClientRepository;
+import com.example.demo.dao.CompteRepository;
+import com.example.demo.dao.OperationRepository;
+import com.example.demo.entity.Client;
+import com.example.demo.entity.Compte;
+import com.example.demo.entity.CompteCourant;
+import com.example.demo.entity.CompteEpargne;
+import com.example.demo.entity.Operation;
+import com.example.demo.entity.Retrait;
+import com.example.demo.entity.Versement;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private ClientRepository clientRepository;
+	@Autowired
+	private CompteRepository compteRepository;
+	@Autowired
+	private OperationRepository operationRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+		//Test Client
+		Client client = clientRepository.save(new Client("Nom 1", "email1@gmail.com"));
+		Client client2 = clientRepository.save(new Client("Nom 2", "email2@gmail.com"));
+		
+		//Test compte
+		Compte compte = compteRepository.save(new CompteCourant("CC001", new Date(), 9999, client2, 0.0));
+		Compte compte2 =compteRepository.save(new CompteEpargne("CE001", new Date(), 1000, client, 0.0));
+		
+		//Test Operation
+		Operation operation = operationRepository.save(new Versement(new Date(), 2500, compte2));
+		Operation operation2 = operationRepository.save(new Retrait(new Date(), 200, compte));
+
+	}
+
+}
